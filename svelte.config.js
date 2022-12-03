f26096c852efe3accf45dc517228417cf481baa6
ॐ8️⃣消費者 <wxzvrd@protonmail.com>
2022-12-03 21:57:25 +0000

    alright, that's enough for today

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
+import preprocess from 'svelte-preprocess'
+import seqPreprocessor from 'svelte-sequential-preprocessor'
+import { preprocessThrelte } from '@threlte/preprocess'
+
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	preprocess: seqPreprocessor([preprocess(), preprocessThrelte()])
 };
 
 export default config;
